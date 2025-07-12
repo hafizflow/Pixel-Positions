cebd809f7bcef36b2ccedc243ab934c7a9f43ca7
hafizflow <hafizur.rahman.cs@gmail.com>
2025-07-12 06:49:43 +0600

    UI in Progress

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div>
+        <div class="text-sm text-left">Bongo Academy</div>
+
+        <div class="py-8 font-bold">
+            <h3>Laravel Backend Developer</h3>
+            <p>Full Time - From $12,000</p>
+        </div>
+
+        <div class="flex justify-between items-center mt-auto">
+            <div>
+                <x-tag>Backend</x-tag>
+                <x-tag>Database</x-tag>
+                <x-tag>API</x-tag>
+            </div>
+
+            <img src="" alt="" class="rounded-xl">
+        </div>
+    </div>
+</div>
