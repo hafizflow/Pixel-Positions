92aba016c8b8e5f15d0ba3e744b8e52b023a69cc
hafizflow <hafizur.rahman.cs@gmail.com>
2025-07-20 04:02:35 +0600

    Table Created

---
+++ app/Models/Job.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\JobFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Job extends Model
+{
+    /** @use HasFactory<JobFactory> */
+    use HasFactory;
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tag() {
+
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
